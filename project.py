a6e33d3faae2d5a839431e9579510fca8e7a16ce
Srinivas-VJ <srinivasvj01@gmail.com>
2022-03-26 12:40:28 +0530

    preparing dataset

--- project.py
+++ project.py
@@ -1,62 +1,60 @@
 from matplotlib import pyplot as plt
 from cv2 import cv2
 import numpy as np
+from os import listdir
 
 
 # read image as it is
 def read_img(file_name):
-	img = cv2.imread(file_name)
-	return img
+    img = cv2.imread(file_name)
+    return img
 
 
 # resize image with fixed aspect ratio
 def resize_img(image, scale):
-	res = cv2.resize(image, None, fx=scale, fy=scale, interpolation = cv2.INTER_AREA)
-	return res
+    res = cv2.resize(image, None, fx=scale, fy=scale,
+                     interpolation=cv2.INTER_AREA)
+    return res
 
 
 # convert image to grayscale
 def img_to_gray(image):
-	img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-	return img_gray
+    img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    return img_gray
 
 
 # median blur
 def median_blur(image):
-	blurred_img = cv2.medianBlur(image, 3)
-	return blurred_img
-
+    blurred_img = cv2.medianBlur(image, 3)
+    return blurred_img
 
 
 def adaptive_thresh(image):
-	img_thresh = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 8)
-	# cv2.adaptiveThreshold(src, maxValue, adaptiveMethod, thresholdType, blockSize, C[, dst]) → dsta
-	return img_thresh
-
-
-
-
+    img_thresh = cv2.adaptiveThreshold(
+        image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 8)
+    # cv2.adaptiveThreshold(src, maxValue, adaptiveMethod, thresholdType, blockSize, C[, dst]) → dsta
+    return img_thresh
 
 
 # calculate scale and fit into display
 def display(window_name, image):
-	screen_res = 960, 540	
-	
-	scale_width = screen_res[0] / image.shape[1]
-	scale_height = screen_res[1] / image.shape[0]
-	scale = min(scale_width, scale_height)
-	window_width = int(image.shape[1] * scale)
-	window_height = int(image.shape[0] * scale)
+    screen_res = 960, 540
 
-	# reescale the resolution of the window
-	cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-	cv2.resizeWindow(window_name, window_width, window_height)
+    scale_width = screen_res[0] / image.shape[1]
+    scale_height = screen_res[1] / image.shape[0]
+    scale = min(scale_width, scale_height)
+    window_width = int(image.shape[1] * scale)
+    window_height = int(image.shape[0] * scale)
 
-	# display image
-	cv2.imshow(window_name, image)
-	# wait for any key to quit the program
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+    # reescale the resolution of the window
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(window_name, window_width, window_height)
+
+    # display image
+    cv2.imshow(window_name, image)
+    # wait for any key to quit the program
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 max_val = 8
@@ -78,53 +76,47 @@ display('Input Processed Image', original)
 # (kp1, des1) = orb.detectAndCompute(test_img, None)
 (kp1, des1) = orb.detectAndCompute(test_img, None)
 
-training_set = [
-	'files/Train/india_10_1.jpg', 
-	'files/Train/india_10_2.jpg', 
-	'files/Train/india_20.jpg', 
-	'files/Train/india_50.jpg', 
-	'files/Train/india_100_1.jpg',
-	'files/Train/india_100_2.jpg', 
-	'files/Train/india_200.jpg', 
-	'files/Train/india_500.jpg', 
-	'files/Train/india_2000.jpg',
-	  ]
+train_path = 'files/Train/'
+training_set = []
+for f in listdir(train_path):
+    training_set.append(train_path + f)
+print(training_set)
 
 
 for i in range(0, len(training_set)):
-	# train image
-	train_img1 = cv2.imread(training_set[i])
-	train_img = cv2.cvtColor(train_img1, cv2.COLOR_BGR2GRAY) 
+    # train image
+    train_img1 = cv2.imread(training_set[i])
+    train_img = cv2.cvtColor(train_img1, cv2.COLOR_BGR2GRAY)
 
-	(kp2, des2) = orb.detectAndCompute(train_img, None)
+    (kp2, des2) = orb.detectAndCompute(train_img, None)
 
-	# brute force matcher
-	bf = cv2.BFMatcher()
-	all_matches = bf.knnMatch(des1, des2, k=2)
+    # brute force matcher
+    bf = cv2.BFMatcher()
+    all_matches = bf.knnMatch(des1, des2, k=2)
 
-	good = []
-	for (m, n) in all_matches:
-		if m.distance < 0.789 * n.distance:
-			good.append([m])
+    good = []
+    for (m, n) in all_matches:
+        if m.distance < 0.789 * n.distance:
+            good.append([m])
 
-	if len(good) > max_val:
-		max_val = len(good)
-		max_pt = i
-		max_kp = kp2
+    if len(good) > max_val:
+        max_val = len(good)
+        max_pt = i
+        max_kp = kp2
 
-	print(i, ' ', training_set[i], ' ', len(good))
+    print(i, ' ', training_set[i], ' ', len(good))
 
 if max_val != 8:
-	print(training_set[max_pt])
-	print('good matches ', max_val)
+    print(training_set[max_pt])
+    print('good matches ', max_val)
 
-	train_img = cv2.imread(training_set[max_pt])
-	img3 = cv2.drawMatchesKnn(test_img, kp1, train_img, max_kp, good, 4)
-	
-	note = str(training_set[max_pt])[12:-4]
-	print('\nDetected note: ', note)
-	(plt.imshow(img3), plt.show())
+    train_img = cv2.imread(training_set[max_pt])
+    img3 = cv2.drawMatchesKnn(test_img, kp1, train_img, max_kp, good, 4)
+
+    note = str(training_set[max_pt])[12:-4]
+    print('\nDetected note: ', note)
+    (plt.imshow(img3), plt.show())
 
 
 else:
-	print('No Matches')
+    print('No Matches')
